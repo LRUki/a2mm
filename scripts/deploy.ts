3d95e966db26d7ad9271ffafdfef1b072543380d
leo <leounoki22@gmail.com>
2021-10-21 23:36:31 +0100

    modify deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,19 @@
 // deploys the smart contract to the cahin
 // refert to hardhat.config.ts
 import { ethers, upgrades } from "hardhat";
+
 async function deploy() {
   const Box = await ethers.getContractFactory("Box");
   console.log("Deploying Box...");
   const box = await upgrades.deployProxy(Box, [1000], {
     initializer: "store",
   });
-  await box.deployed();
+
+  try {
+    await box.deployed();
+  } catch (err) {
+    console.log("NOT DEPLOYED", err);
+  }
   console.log("Box deployed to:", box.address);
 }
 
