0969d7838f5c7ee16ab488272e2892a8fe9d20e7
leo <leounoki22@gmail.com>
2021-10-25 12:20:10 +0100

    fork mainnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,12 +3,19 @@
  */
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
-
 import { mnemonic, url } from "./secret.json";
 
 export default {
-  solidity: "0.8.4",
+  solidity: "0.7.6",
+  defaultNetwork: "hardhat",
   networks: {
+    hardhat: {
+      //https://hardhat.org/hardhat-network/guides/mainnet-forking.html
+      forking: {
+        url: "https://speedy-nodes-nyc.moralis.io/0786eddbee701c23817a1112/eth/mainnet/archive",
+        blockNumber: 13482613,
+      },
+    },
     ropsten: {
       url: url,
       accounts: { mnemonic: mnemonic },
