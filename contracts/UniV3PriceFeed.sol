318726c74d2e04fef80e187f7a4efe0d212c5aa2
leo <leounoki22@gmail.com>
2021-11-01 09:41:27 +0000

    remove init

--- contracts/UniV3PriceFeed.sol
+++ contracts/UniV3PriceFeed.sol
@@ -6,33 +6,26 @@ import "./libraries/TokenAddrs.sol";
 import "hardhat/console.sol";
 
 contract UniV3PriceFeed {
-  IQuoter public quoter;
+  IQuoter public quoter = IQuoter(address(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6));
   using TokenAddrs for TokenAddrs.Token;
-
-  function init() public {
-    quoter = IQuoter(address(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6));
-  }
 
   function getAddres(TokenAddrs.Token token) external pure returns (address) {
     return token.getAddr();
   }
 
   // do not used on-chain, gas inefficient!
-  function getPrice(uint256 amount) external payable returns (uint256) {
-    address tokenIn = TokenAddrs.Token.WETH.getAddr();
-    address tokenOut = TokenAddrs.Token.UNI.getAddr();
-    console.log("TOKENIN",tokenIn);
-    console.log("TOKENOUT",tokenOut);
+  function getPrice(TokenAddrs.Token tokenIn, TokenAddrs.Token tokenOut, uint256 amount) external payable returns (uint256) {
+    console.log("TOKENIN:",tokenIn.getAddr());
+    console.log("TOKENOUT:",tokenOut.getAddr());
     uint24 fee = 3000;
-    uint160 sqrtPriceLimitX96 = 0;
     uint256 x = quoter.quoteExactInputSingle(
-        tokenIn,
-        tokenOut,
+        tokenIn.getAddr(),
+        tokenOut.getAddr(),
         fee,
         amount,
-        sqrtPriceLimitX96
+        0
       );
-    console.log(x);
+    console.log("VALUE=",x);
     return x;
   }
 }
