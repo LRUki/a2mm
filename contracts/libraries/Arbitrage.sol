be4fb4acc6a6079385ae8a68be4d645be11e3a61
Mihey Matev <mm2019@ic.ac.uk>
2021-11-12 15:01:16 +0000

    arbitrage should be done

--- contracts/libraries/Arbitrage.sol
+++ contracts/libraries/Arbitrage.sol
@@ -42,9 +42,10 @@ library Arbitrage {
         }
 
         flashLoanRequiredAmount = 0;
+        uint256[] memory ySplits;
 
         //Level until the union of ML and MR is sortedAmmIndices.
-        while (arbHelper.l + 1 < arbHelper.r) {
+        while (arbHelper.l < arbHelper.r) {
             arbHelper.dyBar = SharedFunctions.howMuchYToSpendToLevelAmms(arbHelper.ML, amms[arbHelper.sortedAmmIndices[arbHelper.l + 1]]);
 
             //The amount we would need to spend, in terms of X, to level out the aggregated AMMs ML with the next
@@ -55,21 +56,34 @@ library Arbitrage {
             // Note that if the commission fee is not 0.3% (or the formula is not xy=k), then this would differ:
             arbHelper.dy = (1000 * arbHelper.ML.x * arbHelper.ML.y - 1000 * arbHelper.ML.y * arbHelper.dxBar) / (997 * arbHelper.dxBar);
 
-            uint256[] memory ySplits;
             //TODO: for some reason we can't pass array slices as function arguments, so have to create an array with the slice each time. Can this be done more efficiently?
+            //Create arrays which hole the left AMMs and right AMMs. These are the ones which have been leveled within
+            // their respective array.
             Structs.Amm[] memory sortedAmmsUpTol = new Structs.Amm[](amms.length - arbHelper.r);
             for (uint256 k = 0; k <= arbHelper.l; ++k) {
                 sortedAmmsUpTol[k - arbHelper.r] = arbHelper.sortedAmms[k];
             }
+            Structs.Amm[] memory sortedAmmsrToEnd = new Structs.Amm[](amms.length - arbHelper.r);
+            for (uint256 k = arbHelper.r; k < amms.length; ++k) {
+                sortedAmmsrToEnd[k - arbHelper.r] = arbHelper.sortedAmms[k];
+            }
 
-            if (arbHelper.dyBar < arbHelper.dy) {
+            if (arbHelper.r - arbHelper.l == 1) {
+                //If the union of the left and right arrays is all of the AMMs, then the last step is to just
+                // arbitrage on their aggregates.
+                uint256 dyOpt = _optimalAmountToSpendOnArbitrageForY(arbHelper.ML, arbHelper.MR);
+                ySplits = SharedFunctions.howToSplitRoutingOnLeveledAmms(sortedAmmsUpTol, dyOpt);
+            } else if (arbHelper.dyBar < arbHelper.dy) {
+                //Need to level the left AMMs, as the cost of leveling the right ones would be higher
                 ySplits = SharedFunctions.howToSplitRoutingOnLeveledAmms(sortedAmmsUpTol, arbHelper.dyBar);
                 arbHelper.increaseLow = true;
             } else {
+                //Need to level the right AMMs, as the cost of leveling the left ones would be higher
                 ySplits = SharedFunctions.howToSplitRoutingOnLeveledAmms(sortedAmmsUpTol, arbHelper.dy);
                 arbHelper.increaseLow = false;
             }
 
+            //Route the difference in y needed to level the AMMs (to get some X) on the left AMMs (ML)
             uint256 xGainSum = 0;
             for (uint256 k = 0; k <= arbHelper.l; ++k) {
                 uint256 xGain = SharedFunctions.quantityOfXForY(arbHelper.sortedAmms[k], ySplits[k]);
@@ -85,12 +99,7 @@ library Arbitrage {
                 }
             }
 
-            //TODO: for some reason we can't pass array slices as function arguments, so have to create an array with the slice each time. Can this be done more efficiently?
-            Structs.Amm[] memory sortedAmmsrToEnd = new Structs.Amm[](amms.length - arbHelper.r);
-            for (uint256 k = arbHelper.r; k < amms.length; ++k) {
-                sortedAmmsrToEnd[k - arbHelper.r] = arbHelper.sortedAmms[k];
-            }
-
+            //Then we also route the x we gained to the right AMMs (MR)
             uint256[] memory xSplits = SharedFunctions.howToSplitRoutingOnLeveledAmms(sortedAmmsrToEnd, xGainSum);
             for (uint256 k = arbHelper.r; k < amms.length; ++k) {
                 uint256 yGain = SharedFunctions.quantityOfXForY(arbHelper.sortedAmms[k], xSplits[k - arbHelper.r]);
@@ -99,26 +108,27 @@ library Arbitrage {
                 amms[arbHelper.sortedAmmIndices[k]].y -= yGain;
             }
 
+            //Depending on which value was lowest at the start, we either move 'l' up, or 'r' down, and update
+            // aggregate pools
             if (arbHelper.increaseLow) {
-                ++arbHelper.l;
+                arbHelper.ML.x += amms[arbHelper.sortedAmmIndices[++arbHelper.l]].x;
+                arbHelper.ML.y += amms[arbHelper.sortedAmmIndices[arbHelper.l]].y;
             } else {
-                --arbHelper.r;
+                assert(arbHelper.r > 0);
+                arbHelper.MR.x += amms[arbHelper.sortedAmmIndices[--arbHelper.r]].x;
+                arbHelper.MR.y += amms[arbHelper.sortedAmmIndices[arbHelper.r]].y;
             }
         }
-
-        //Finally, we also need to level ML and MR themselves:
-
     }
 
 
     //(Appendix B, formula 22)
-    //Tells us if an arbitrage opportunity is profitable (without considering transaction fees).
-    // @notice - possible overflow;
-    //this formula is inexact. Making it exact might have a higher gas fee, so might be worth investigating if the higher potential profit covers the potentially higher gas fee;
-    //this formula would be different if the commission fee is not 0.3% or the pricing formula is not the constant product exchange formula;
-    // @param amm1 - the first of two AMMs we are considering to do arbitrage on
-    // @param amm2 - the second of two AMMs we are considering to do arbitrage on
-    // @return - whether it is profitable to do arbitrage between these two AMMs; whether Y is 'cheap enough' on amm1
+    // @notice - possible underflow/overflow; \
+    // This formula would be different if the commission fee is not 0.3% or the pricing formula is not the constant  \
+    // product exchange formula; this formula is inexact. Making it exact might have a higher gas fee, so might be  \
+    // worth investigating if the higher potential profit covers the potentially higher gas fee
+    // @param amm1 - The AMM which we are selling X on
+    // @param amm2 - The AMM which we are buying X on
     function _isArbitrageProfitable(Structs.Amm memory amm1, Structs.Amm memory amm2) private pure returns (bool) {
         return 500 * amm2.y * amm1.x < 497 * amm1.y * amm2.x;
     }
@@ -129,10 +139,10 @@ library Arbitrage {
     // This formula would be different if the commission fee is not 0.3% or the pricing formula is not the constant  \
     // product exchange formula; this formula is inexact. Making it exact might have a higher gas fee, so might be  \
     // worth investigating if the higher potential profit covers the potentially higher gas fee
-    // @param t1_1 - the first of two AMMs we are considering to do arbitrage on; buying Y is cheaper here
-    // @param t2_1 - the first of two AMMs we are considering to do arbitrage on; buying Y is cheaper here
-    // @param t1_2 - the first of two AMMs we are considering to do arbitrage on; buying Y is cheaper here
-    // @param t2_2 - the second of two AMMs we are considering to do arbitrage on; buying Y is more expensive here
+    // @param t1_1 - The amount of liquidity of token 't1' on the first AMM
+    // @param t2_1 - The amount of liquidity of token 't2' on the first AMM
+    // @param t1_2 - The amount of liquidity of token 't1' on the second AMM
+    // @param t2_2 - The amount of liquidity of token 't2' on the second AMM
     // @return dXOpt - the optimal amount we should wager on the arbitrage for optimal profit
     function _optimalAmountToSpendOnArbitrage(uint256 t1_1, uint256 t2_1, uint256 t1_2, uint256 t2_2) private pure returns (uint256) {
         assert(t2_1 * t1_2 > t2_2 * t1_1);
@@ -140,14 +150,6 @@ library Arbitrage {
     }
 
 
-    //(Appendix B, formula 20)
-    // @notice - possible underflow/overflow; \
-    // This formula would be different if the commission fee is not 0.3% or the pricing formula is not the constant  \
-    // product exchange formula; this formula is inexact. Making it exact might have a higher gas fee, so might be  \
-    // worth investigating if the higher potential profit covers the potentially higher gas fee
-    // @param amm1 - the first of two AMMs we are considering to do arbitrage on; buying Y is cheaper here
-    // @param amm2 - the second of two AMMs we are considering to do arbitrage on; buying Y is more expensive here
-    // @return dXOpt - the optimal amount we should wager on the arbitrage for optimal profit
     function _optimalAmountToSpendOnArbitrageForX(Structs.Amm memory amm1, Structs.Amm memory amm2) private pure returns (uint256) {
         require(amm1.y * amm2.x > amm2.y * amm1.x, 'Buying Y must be cheaper on amm1, however it is not!');
         return _optimalAmountToSpendOnArbitrage(amm1.x, amm1.y, amm2.x, amm2.y);
