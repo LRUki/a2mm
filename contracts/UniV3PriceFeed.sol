7866d2d6213937e6518f15cdef8fbcb38bc0c2bd
leo <leounoki22@gmail.com>
2021-11-22 02:16:51 +0000

    reformat, move from swap to dex provider

--- contracts/UniV3PriceFeed.sol
+++ contracts/UniV3PriceFeed.sol
@@ -5,21 +5,37 @@ import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 import "./libraries/TokenAddrs.sol";
 
 contract UniV3PriceFeed {
-  IQuoter public quoter = IQuoter(address(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6));
-  using TokenAddrs for TokenAddrs.Token;
-  event GetPrice(address tokenIn, address tokenOut, uint256 amountIn, uint256 amountOut);
-  // do not used on-chain, gas inefficient!
-  function getPrice(TokenAddrs.Token tokenIn, TokenAddrs.Token tokenOut, uint256 amountIn) external payable returns (uint256) {
-    uint24 fee = 3000;
+    IQuoter public quoter =
+        IQuoter(address(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6));
+    using TokenAddrs for TokenAddrs.Token;
+    event GetPrice(
+        address tokenIn,
+        address tokenOut,
+        uint256 amountIn,
+        uint256 amountOut
+    );
 
-    uint256 amountOut = quoter.quoteExactInputSingle(
-        tokenIn.getAddr(),
-        tokenOut.getAddr(),
-        fee,
-        amountIn,
-        0
-      );
-    emit GetPrice(tokenIn.getAddr(), tokenOut.getAddr(), amountIn, amountOut);
-    return amountOut;
-  }
+    // do not used on-chain, gas inefficient!
+    function getPrice(
+        TokenAddrs.Token tokenIn,
+        TokenAddrs.Token tokenOut,
+        uint256 amountIn
+    ) external payable returns (uint256) {
+        uint24 fee = 3000;
+
+        uint256 amountOut = quoter.quoteExactInputSingle(
+            tokenIn.getAddr(),
+            tokenOut.getAddr(),
+            fee,
+            amountIn,
+            0
+        );
+        emit GetPrice(
+            tokenIn.getAddr(),
+            tokenOut.getAddr(),
+            amountIn,
+            amountOut
+        );
+        return amountOut;
+    }
 }
--- contracts/libraries/TokenAddrs.sol
+++ contracts/libraries/TokenAddrs.sol
@@ -4,27 +4,27 @@ pragma solidity 0.8.3;
 //token addresses on ETH mainchain from https://info.uniswap.org/#/pools
 
 library TokenAddrs {
-  enum Token {
-    WETH,
-    UNI,
-    DAI,
-    USDT,
-    AXS
-  }
-
-  function getAddr(Token token) external pure returns (address) {
-    if (token == Token.WETH) {
-      return address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    } else if (token == Token.UNI) {
-      return address(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
-    } else if (token == Token.DAI) {
-      return address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    } else if (token == Token.USDT) {
-      return address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-    } else if (token == Token.AXS) {
-      return address(0xBB0E17EF65F82Ab018d8EDd776e8DD940327B28b);
-    } else {
-      revert("Invalid TOKEN");
+    enum Token {
+        WETH,
+        UNI,
+        DAI,
+        USDT,
+        AXS
     }
-  }
+
+    function getAddr(Token token) external pure returns (address) {
+        if (token == Token.WETH) {
+            return address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+        } else if (token == Token.UNI) {
+            return address(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+        } else if (token == Token.DAI) {
+            return address(0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        } else if (token == Token.USDT) {
+            return address(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+        } else if (token == Token.AXS) {
+            return address(0xBB0E17EF65F82Ab018d8EDd776e8DD940327B28b);
+        } else {
+            revert("Invalid TOKEN");
+        }
+    }
 }
